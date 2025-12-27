710a79ba3b96953e41a4db5ee6a6653022dab468
Atakan <atakansolak28@hotmail.com>
2025-12-28 01:01:34 +0300

    fix: correct env usage

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
 import type { NextConfig } from "next";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+if (!supabaseUrl) {
+  throw new Error(
+    "NEXT_PUBLIC_SUPABASE_URL is not defined in your environment variables.",
+  );
+}
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "kvpgipqcdweseityoodx.storage.supabase.co",
-        port: "",
-        pathname: "/storage/v1/object/public/**",
-      },
-      {
-        protocol: "https",
-        hostname: "kvpgipqcdweseityoodx.supabase.co",
+        hostname: new URL(supabaseUrl).hostname,
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
